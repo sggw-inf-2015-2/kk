9b70f250ac38344ecf7d6b2c44f49cf1df91226c
tomczynskiJaroslaw <tomczynskijaroslaw@gmail.com>
2017-05-23 22:29:05 +0200

    Add files via upload

--- src/user.cpp
+++ src/user.cpp
@@ -1,17 +1,117 @@
 #include "user.h"
+QList<User*> User::registeredUsers;
 
-User::User(QObject *parent) : QObject(parent)
+User::User(QString firstName, QString lastName)
 {
+    this->firstName = firstName;
+    this->lastName = lastName;
 
 }
 
-static void User::addUser(QString firstName, QString lastName)
+User::User(QString firstName, QString lastName, gender personGender)
 {
-    QUuid newId = new QUuid();
-    registeredUsers.append(User());
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->personGender=personGender;
+    //QUuid newId = new QUuid();
+}
+
+User::User(QString firstName, QString lastName, gender personGender,double score)
+{
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->personGender=personGender;
+    this->shoutScore=score;
+    //QUuid newId = new QUuid();
+}
+
+float User::getShoutScore()
+{
+    return shoutScore;
+}
+
+void User::exportToCSV()
+{
+   QString filename = "Data.csv";
+   QFile file(filename);
+   if (file.open(QIODevice::ReadWrite)) {
+   QTextStream stream(&file);
+    foreach (User* u, User::registeredUsers) {
+        stream << u->firstName<<","<<u->lastName<<","<<u->personGender<<","<<u->shoutScore <<""<< endl;
+    }
+   }
+}
+
+QList<User*> User::importFromCSV()
+{
+    QFile file("Data.csv");
+    if(!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while(!in.atEnd()) {
+        QString line = in.readLine();
+        QStringList fields = line.split(",");
+        QString firstName = fields.at(0);
+        QString lastName = fields.at(1);
+        gender personGender;
+//        if (fields.at(2)=="1"){
+//            personGender = 1;
+//        }
+        int score = fields.at(3).toInt();
+        User* user = new User(firstName,lastName,personGender,score);
+        User::registeredUsers.append(user);
+    }
+
+    file.close();
+    return User::registeredUsers;
+}
+
+void User::TEST()
+{
+
+
+//     User::createUser("Jan","Kowalski",false);
+//     User::createUser("Ala","Nowak",true);
+     //User::createUser("Arek","Zot",false);
+
+//     User::exportToCSV();
+     User::importFromCSV();
+     QTextStream out(stdout);
+     out<<"--->"<<registeredUsers.count()<<"<---";
+     out<<registeredUsers.at(0)->firstName<<endl;
+     out<<registeredUsers.at(1)->firstName<<endl;
+     out<<registeredUsers.at(2)->firstName<<endl;
+
+     //User::editUser(0,"OLEK","Janecki",true);
+     out<<registeredUsers.at(0)->firstName<<endl;
+     out<<registeredUsers.at(0)->lastName<<endl;
+     out<<registeredUsers.at(0)->shoutScore<<endl;
+     User::exportToCSV();
+//     foreach (User* u, User::registeredUsers) {
+//         out<<(u->firstName);
+//     }
 }
 
 void User::setShoutScore(float score)
 {
     shoutScore = score;
+}
+
+
+
+void User::createUser(QString firstName, QString lastName, gender personGender)
+{
+    User* user = new User(firstName,lastName,personGender);
+    User::registeredUsers.append(user);
+}
+
+void User::editUser(int ID, QString firstName, QString lastName, gender personGender)
+{
+    User* u = User::registeredUsers.at(0);
+    u->firstName = firstName;
+    u->lastName = lastName;
+    u->personGender = personGender;
 }
--- src/user.h
+++ src/user.h
@@ -1,35 +1,47 @@
 #ifndef USER_H
 #define USER_H
-
 #include <QObject>
+#include <QString>
 #include <QUuid>
+#include <Qfile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <String>
 
 class User : public QObject
 {
     Q_OBJECT
+    enum gender {kobieta=0,mezczyzna=1};
+    private:
+        static QList<User*> registeredUsers;
+        QString firstName;
+        QString lastName;
+        gender personGender;
+        double shoutScore;
+        bool attemptedRepeat;
+        //QUuid id;
+        //int id;
+    public:
 
-    static QList<User> registeredUsers;
+        QUuid getId();
+        User(QString firstName,QString lastName);
+        User(QString firstName,QString lastName,gender personGender);
+        User(QString firstName, QString lastName, gender gender,double score);
+        void setShoutScore(float score);
+        //void addUserToList();
+        //QUuid getId();
+        static void createUser(QString firstName,QString lastName,gender personGender);
+        static void editUser(int ID,QString firstName, QString lastName, gender personGender);
+        float getShoutScore();
+        void setScore(double score);
+        static void exportToCSV();
+        static QList<User*> importFromCSV();
+        static void TEST();
+        //QList<User> getAllUsers();
+        //void repeat();
+    signals:
 
-    QString firstName;
-    QString lastName;
-    float shoutScore;
-    QUuid id;
-
-public:
-    explicit User(QObject *parent = 0);
-
-    static void addUser(QString firstName, QString lastName);
-    static void removeUser(QUuid id);
-
-    QUuid getId();
-    float getShoutScore();
-
-    void save();
-
-signals:
-
-public slots:
-    void setShoutScore(float score);
+    public slots:
 };
 
 #endif // USER_H
