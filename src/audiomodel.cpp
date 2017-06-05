b72fd485cd715848aa273cebb4190cdd21562396
Adrian Borucki <gentoolx@gmail.com>
2017-06-05 17:03:38 +0200

    Add convolve and cconvolve
    - Implement `convolve` and `cconvolve` based on the given auxiliary
      materials from Moodle.
    - Add `fft` and `ifft` declarations. No implementation done.

--- src/audiomodel.cpp
+++ src/audiomodel.cpp
@@ -1,7 +1,49 @@
 #include "audiomodel.h"
 
+const complex<double> AudioModel::ZERO = complex<double>(0, 0);
+
 AudioModel::AudioModel(QObject *parent) : QObject(parent)
 {
 
 }
 
+QVector<complex<double>> AudioModel::convolve(QVector<std::complex<double> > x, QVector<std::complex<double> > y)
+{
+    QVector<complex<double>> a = QVector<complex<double>>(2*x.length());
+    for (int i = 0; i < x.length(); i++)
+        a[i] = x[i];
+    for (int i = x.length(); i < 2*x.length(); i++)
+        a[i] = ZERO;
+
+    QVector<complex<double>> b = QVector<complex<double>>(2*y.length());
+    for (int i = 0; i < y.length(); i++)
+        b[i] = y[i];
+    for (int i = y.length(); i < 2*y.length(); i++)
+        b[i] = ZERO;
+
+    return cconvolve(a, b);
+}
+
+QVector<complex<double>> AudioModel::cconvolve(QVector<std::complex<double> > x, QVector<std::complex<double> > y)
+{
+    // TODO: should probably pad x and y with 0s so that they have same length
+    // and are powers of 2
+    if (x.length() != y.length()) {
+       // throw new IllegalArgumentException("Dimensions don't agree");
+    }
+
+    int N = x.length();
+
+    // compute FFT of each sequence
+    QVector<complex<double>> a = fft(x);
+    QVector<complex<double>> b = fft(y);
+
+    // point-wise multiply
+    QVector<complex<double>> c = QVector<complex<double>>(N);
+    for (int i = 0; i < N; i++) {
+        c[i] = a[i] * b[i];
+    }
+
+    // compute inverse FFT
+    return ifft(c);
+}
--- src/audiomodel.h
+++ src/audiomodel.h
@@ -2,13 +2,24 @@
 #define AUDIOMODEL_H
 
 #include <QObject>
+#include <QVector>
+#include <complex>
+
+using namespace std;
 
 class AudioModel : public QObject
 {
     Q_OBJECT
+
+    static const complex<double> ZERO;
 public:
     explicit AudioModel(QObject *parent = 0);
 
+    QVector<complex<double>> convolve(QVector<complex<double>> zs, QVector<complex<double>> qs);
+    QVector<complex<double>> cconvolve(QVector<complex<double>> zs, QVector<complex<double>> qs);
+
+    QVector<complex<double>> fft(QVector<complex<double>> x);
+    QVector<complex<double>> ifft(QVector<complex<double>> x);
 signals:
 
 public slots:
