d0bcff2d768a869fb20c25ed4f1a491425d2b3c4
kamilwu <kami36@poczta.fm>
2017-06-08 14:04:50 +0200

    Many minor GUI improvements

--- src/userwindow.h
+++ src/userwindow.h
@@ -1,23 +1,23 @@
 #ifndef USERWINDOW_H
 #define USERWINDOW_H
 
-#include <QWidget>
+#include <QMainWindow>
 
 namespace Ui {
 class UserWindow;
 }
 
-class UserWindow : public QWidget
+class UserWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit UserWindow(QWidget *parent = 0);
     ~UserWindow();
+    void resizeEvent(QResizeEvent *event) override;
 
 private:
     Ui::UserWindow *ui;
-    int counter;
 };
 
 #endif // USERWINDOW_H
