9653e3c297f0af02023477f11994b8cc9555088b
kamilwu <kami36@poczta.fm>
2017-06-18 12:27:38 +0200

    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 #include "userwindow.h"
 #include <QApplication>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+	QTranslator qtTranslator;
+	if (qtTranslator.load(QLocale::system(), "qt", "_",
+						  QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+		a.installTranslator(&qtTranslator);
+
     UserWindow uw;
     MainWindow w(&uw);
     w.show();
